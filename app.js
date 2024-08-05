672cc824ca9f5f463f9f3b068639dd2df22ce698
wfishburn1 <william.fishburn@snhu.edu>
2024-08-04 20:09:43 -0400

    Module 5 completed baseline

--- app.js
+++ app.js
@@ -7,14 +7,17 @@ var logger = require('morgan');
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
 var travelRouter = require('./app_server/routes/travel');
+var apiRouter = require('./app_api/routes/index');
+
 var handlebars = require('hbs');
 
-var app = express();
+//Bring in the database
+require('./app_api/models/db');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'app_server', 'views'));
 
 // register handlbars partials (https://www.npmjs.com/package/hbs)
 handlebars.registerPartials(__dirname + '/app_server/views/partials');
@@ -30,6 +33,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/travel', travelRouter);
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- app_api/models/db.js
+++ app_api/models/db.js
@@ -2,12 +2,14 @@ const mongoose = require('mongoose');
 const host = process.env.DB_HOST || '127.0.0.1';
 const dbURI = `mongodb://${host}/travlr`;
 const readLine = require('readline');
+
 // Build the connection string and set the connection timeout.
 // timeout is in milliseconds.
 const connect = () => {
 setTimeout(() => mongoose.connect(dbURI, {
-}), 5000);
+}), 1000);
 }
+
 // Monitor connection events
 mongoose.connection.on('connected', () => {
 console.log(`Mongoose connected to ${dbURI}`);
